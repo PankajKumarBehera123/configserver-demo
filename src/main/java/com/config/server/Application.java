69bcb4a775c0b8b288e84507a2d051f5eaaaf504
PankajKumarBehera123 <53708210+PankajKumarBehera123@users.noreply.github.com>
2021-05-27 23:29:25 +0530

    mmmmmmmmmmmmm

--- src/main/java/com/config/server/Application.java
+++ src/main/java/com/config/server/Application.java
@@ -12,6 +12,7 @@ public class Application {
 	public static void main(String[] args) {
 		ApplicationContext ctx=SpringApplication.run(Application.class, args);
 		//something from others
+		int a=10
 		
 	}
 
